9526755689ec6bdd4ef1688a044b7a2a867e566d
oberbrjj <joberbr@hotmail.com>
2021-12-02 05:34:20 -0700

    add defeated by method

--- actors.py
+++ actors.py
@@ -1,10 +1,14 @@
 class Roll:
-    def __init__(self, name, defeat):
+    def __init__(self, name, defeat, lose):
         self.name = name
         self.defeat = defeat
+        self.lose = lose
 
     def can_defeat(self, roll):
         return roll.name == self.defeat
+
+    def defeated_by(self, roll):
+        return roll.name == self.lose
 
 
 class Player:
